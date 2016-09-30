eb1e71c197c421515909d9f90b1a1a6f7f6c2361
Lucas Tormin <lucasntormin@gmail.com>
2016-09-30 17:23:53 -0300

    Adicionadas mensagens de exceção para quando as exceções definidas no projeto forem lançadas.

--- Source Code Inspection/src/br/mackenzie/ticketmachine/exception/PapelMoedaInvalidaException.java
+++ Source Code Inspection/src/br/mackenzie/ticketmachine/exception/PapelMoedaInvalidaException.java
@@ -5,9 +5,14 @@ package br.mackenzie.ticketmachine.exception;
  * @author Calebe de Paula Bianchini
  */
 public class PapelMoedaInvalidaException extends Exception {
-
+    private final String MESSAGE="Valor de papel moeda inserido contém um valor fora dos parâmetros permitidos. Os valores permitidos são 2, 5, 10, 50, 100.";
     public PapelMoedaInvalidaException() {
         System.out.println("Erro:" + getMessage());
+    }
+
+    @Override
+    public String getMessage() {
+        return this.MESSAGE; //To change body of generated methods, choose Tools | Templates.
     }
 
 }
--- Source Code Inspection/src/br/mackenzie/ticketmachine/exception/SaldoInsuficienteException.java
+++ Source Code Inspection/src/br/mackenzie/ticketmachine/exception/SaldoInsuficienteException.java
@@ -5,8 +5,14 @@ package br.mackenzie.ticketmachine.exception;
  * @author Calebe de Paula Bianchini
  */
 public class SaldoInsuficienteException extends Exception {
-
+    private final String MESSAGE="O saldo inserido é inferior ao valor do ticket.";
     public SaldoInsuficienteException() {
         System.out.println("Erro:" + getMessage());
     }
+
+    @Override
+    public String getMessage() {
+        return this.MESSAGE; //To change body of generated methods, choose Tools | Templates.
+    }
+    
 }
